c4979af515332a6263ce2012e6f21cd9d07eeb7f
Dmytro Kozyriev <Kozyriev@ukr.net>
2019-04-25 12:04:44 -0300

    Final version

--- ArrayListSimpleEx/Form1.Designer.cs
+++ ArrayListSimpleEx/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ArrayListSimpleEx
 {
-    partial class Form1
+    partial class btnSortArray
     {
         /// <summary>
         /// Required designer variable.
@@ -104,6 +104,7 @@
             this.Controls.Add(this.btnShowMsg);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- ArrayListSimpleEx/Form1.cs
+++ ArrayListSimpleEx/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Kozyriev Dmitry 2019-04-24 WMAD Jr b
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace ArrayListSimpleEx
 {
-    public partial class Form1 : Form
+    public partial class btnSortArray : Form
     {
-        public Form1()
+        ArrayList message = new ArrayList();
+        bool reverse = true;
+        public btnSortArray()
         {
             InitializeComponent();
         }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            message.Add("I");
+            message.Add("love");
+            message.Add("programming");
+            message.Add("so");
+            message.Add("much");
+
+        }
+
+
+        private void btnShowMsg_Click(object sender, EventArgs e)
+        {
+
+            {
+                lblMessage.Text = string.Empty;
+                string display = "";
+                foreach (string item in message)
+                {
+                    display += item + " ";
+                }
+                lblMessage.Text = display;
+                reverse = true;
+            }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtSecondPos.Text != string.Empty && reverse == true)
+            {
+                string addWord = txtSecondPos.Text.Trim().ToString();
+                message.Insert(1, addWord + " ");
+            }
+
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            reverse = false;
+            lblMessage.Text = "";
+
+            message.Reverse();
+            foreach (string item in message)
+            {
+                lblMessage.Text += item + " ";
+            }
+            message.Reverse();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            message.RemoveAt(1);
+            txtSecondPos.Focus();
+        }
+
     }
 }
